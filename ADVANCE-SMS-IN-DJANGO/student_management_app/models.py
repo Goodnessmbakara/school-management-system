fe19a6c7c90b786d5c3048019e3316609da1044b
Goodnessmbakara <amicablembakara50@gmail.com>
2024-04-06 22:40:43 +0100

    models fix

--- ADVANCE-SMS-IN-DJANGO/student_management_app/models.py
+++ ADVANCE-SMS-IN-DJANGO/student_management_app/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.utils.timezone import now
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class SessionYearModel(models.Model):
@@ -16,6 +18,21 @@ class CustomUser(models.Model):
     user_type = models.CharField(default=1, choices=user_type_data, max_length=10)
 
 
+class AdminHOD(models.Model):
+    id = models.AutoField(primary_key=True)
+    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
+
+class Staffs(models.Model):
+    id = models.AutoField(primary_key=True)
+    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    address = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
+
 class Classes(models.Model):
     LEVEL_CHOICES = (
         ('Nursery', 'Nursery'),
@@ -26,6 +43,18 @@ class Classes(models.Model):
     class_name = models.CharField(max_length=255)
     class_code = models.CharField(max_length=10, unique=True)
     level = models.CharField(max_length=50, choices=LEVEL_CHOICES)
+class Students(models.Model):
+    id = models.AutoField(primary_key=True)
+    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    gender = models.CharField(max_length=50)
+    profile_pic = models.FileField()
+    address = models.TextField()
+    class_id = models.ForeignKey(Classes, on_delete=models.DO_NOTHING)
+    session_year_id = models.ForeignKey(SessionYearModel, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
+
 
 
 class Subject(models.Model):
@@ -57,7 +86,7 @@ class Grade(models.Model):
 
     def calculate_final_grade(self):
         # Implement the logic for final grade calculation here
-        return (self.test1 + self.test2 + self.test3 + self.exam) / 4
+        return (self.test1 + self.test2 + self.test3 + self.exam)
 
 
 class GradeDeadline(models.Model):
@@ -72,3 +101,25 @@ class GradeDeadline(models.Model):
             return now() <= deadline.deadline
         except cls.DoesNotExist:
             return False
+
+
+# Django Signals
+@receiver(post_save, sender=CustomUser)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        if instance.user_type == 1:
+            AdminHOD.objects.create(admin=instance)
+        elif instance.user_type == 2:
+            Staffs.objects.create(admin=instance)
+        elif instance.user_type == 3:
+            Students.objects.create(admin=instance, class_id=Classes.objects.get(id=1), session_year_id=SessionYearModel.objects.get(id=1), address="", profile_pic="", gender="")
+
+
+@receiver(post_save, sender=CustomUser)
+def save_user_profile(sender, instance, **kwargs):
+    if instance.user_type == 1:
+        instance.adminhod.save()
+    elif instance.user_type == 2:
+        instance.staffs.save()
+    elif instance.user_type == 3:
+        instance.students.save()
